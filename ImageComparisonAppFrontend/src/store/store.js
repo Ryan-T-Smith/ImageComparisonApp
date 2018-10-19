890077d2f1dd9b98f68e85a952d8f151257db412
jeremyculler <jeremymculler@gmail.com>
2018-10-19 08:51:29 -0700

    adds default redux logger, resolves TODO

--- ImageComparisonAppFrontend/src/store/store.js
+++ ImageComparisonAppFrontend/src/store/store.js
@@ -1,15 +1,12 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-//TODO create loggerMiddleWare and monitorReducersEnhancer : Jeremy 10/19/18
-// reference: https://redux.js.org/recipes/configuringyourstore
-// import monitorReducersEnhancer from './enhancers/monitorReducers'
-// import loggerMiddleware from '../middleware/logger'
+import logger from 'redux-logger';
 
 import rootReducer from '../reducers/RootReducer';
 
 export const configureStore = (preloadedState) => {
   const middlewares = [
-    // loggerMiddleware,
+    logger,
     thunkMiddleware
   ]
 
@@ -17,7 +14,6 @@ export const configureStore = (preloadedState) => {
 
   const enhancers = [
     middlewareEnhancer,
-    // monitorReducersEnhancer
   ];
 
   const composedEnhancers = compose(...enhancers);
